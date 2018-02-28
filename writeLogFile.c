311fcc0b8f22840125b7a9055a3e86e31a4633b9
jairoM26 <jairomendezmartinez@gmail.com>
2018-02-27 18:05:03 -0600

    Cambio en writeLogFile

--- writeLogFile.c
+++ writeLogFile.c
@@ -31,32 +31,30 @@
  * @param indicate the type of the alert
  * @return return 1 if the message has been write, if not return -1
  */
-int writeLog(char * pAlertUsage, char* pThreshold, int pType){
+void writeLog(char * pAlertUsage, char* pThreshold, int pType){
 
 	FILE* fp = fopen(LOG_FILE_NAME,"a"); //open file
 
 	if(fp == NULL){ //if the file can not be open
-		printf("Error, no file open");
-		return -1;
+		fclose(fp); 
+        exit(EXIT_FAILURE);
 	}
 	//file open
-	else{		
-		if (pType == CPU_ALERT){
-			fprintf(fp, "[CRITICAL] CPU usage is currently: %s which is over %s\n", pAlertUsage, pThreshold);
-			return 1;			
-		} else if (pType == MEM_ALERT) {
-			fprintf(fp, "[CRITICAL] Memory usage is currently: %s which is over %s\n", pAlertUsage, pThreshold);
-			return 1;
-		} else if (pType == NET_ALERT) {
-			fprintf(fp, "[CRITICAL] SYN flood connections detected. Currently there are %s active SYN_RECV connections, which is over: %s\n", pAlertUsage, pThreshold);
-			return 1;
-		} else if (pType == SYS_ERROR_ALERT){
-			fprintf(fp, "[CRITICAL] System critical error has been detected: %s\n", pAlertUsage);
-			return 1;
-		} else {
-			//printf("Type spicifier is not valid");
-			return -1;
-		}
+	else{	
+		switch(pType){
+			case(CPU_ALERT):
+				fprintf(fp, "[CRITICAL] CPU usage is currently: %s which is over %s\n", pAlertUsage, pThreshold);			
+				break;
+			case(MEM_ALERT):
+				fprintf(fp, "[CRITICAL] Memory usage is currently: %s which is over %s\n", pAlertUsage, pThreshold);
+				break;
+			case(NET_ALERT):
+				fprintf(fp, "[CRITICAL] SYN flood connections detected. Currently there are %s active SYN_RECV connections, which is over: %s\n", pAlertUsage, pThreshold);
+				break;
+			case(SYS_ERROR_ALERT):
+				fprintf(fp, "[CRITICAL] System critical error has been detected: %s\n", pAlertUsage);
+				break;
+		}	
 	}
 }
 
